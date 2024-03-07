0dd8922f3198ba1ac56107e44ee5559b3e3ec056
Natthawat Raocharoensinp <chan1sook@hotmail.com>
2024-03-07 13:59:49 +0700

    Fix Logs

--- server/logger.ts
+++ server/logger.ts
@@ -1,17 +1,17 @@
-import "colors";
+import colors from "colors";
 
 export function getCustomLogger(key: string, socket: import('socket.io').Socket) {
   const logDebug = (...msg: any[]) => {
-    console.log(`[${key}]`.blue, ...msg);
+    console.log(colors.blue(`[${key}]`), ...msg);
   }
   const logInfo = (title: string, ...msg: any[]) => {
-    console.log(`[${title || key}]`.magenta, ...msg);
+    console.log(colors.magenta(`[${title || key}]`), ...msg);
   }
   const logSuccess = (title: string, ...msg: any[]) => {
-    console.log(`[${title || key}]`.green, ...msg);
+    console.log(colors.green(`[${title || key}]`), ...msg);
   }
   const logFailed = (title: string, ...msg: any[]) => {
-    console.log(`[${title || key}]`.red, ...msg);
+    console.log(colors.green(`[${title || key}]`), ...msg);
   }
 
   const emitWithLog = (msg: string, evName?:string , title?: string) => {
